405c039f68aa8128b08b35eec21d05659d7766b0
Tiffany <139176913+talph101@users.noreply.github.com>
2024-12-10 14:28:51 -0600

    new git

---
+++ src/main/java/com/Promo_pros/promos_perks_api/PromosPerksApiApplication.java
@@ -0,0 +1,13 @@
+package com.Promo_pros.promos_perks_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromosPerksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromosPerksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Promo_pros/promos_perks_api/PromosPerksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Promo_pros.promos_perks_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromosPerksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
