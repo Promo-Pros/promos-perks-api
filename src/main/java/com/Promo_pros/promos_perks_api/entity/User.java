5fc7dd03f8023543faf9fa78732ebf9b843997a9
Justin Berg <justin.berg1@verizon.com>
2024-12-24 13:40:05 -0600

    Hashed and Salted the password using Bcrypt

--- src/main/java/com/Promo_pros/promos_perks_api/entity/User.java
+++ src/main/java/com/Promo_pros/promos_perks_api/entity/User.java
@@ -1,16 +1,12 @@
 package com.Promo_pros.promos_perks_api.entity;
 
-
 import com.Promo_pros.promos_perks_api.roles.AccountTypes;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Setter
-@Getter
 @Table(name = "users")
 public class User {
     @Id
@@ -24,4 +20,51 @@ public class User {
     @Enumerated
     private AccountTypes status;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMtn() {
+        return mtn;
+    }
+
+    public void setMtn(int mtn) {
+        this.mtn = mtn;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public AccountTypes getStatus() {
+        return status;
+    }
+
+    public void setStatus(AccountTypes status) {
+        this.status = status;
+    }
 }
+
--- src/main/java/com/Promo_pros/promos_perks_api/servicesImpl/UserServiceImpl.java
+++ src/main/java/com/Promo_pros/promos_perks_api/servicesImpl/UserServiceImpl.java
@@ -4,18 +4,25 @@ import com.Promo_pros.promos_perks_api.entity.User;
 import com.Promo_pros.promos_perks_api.repository.UserRepository;
 import com.Promo_pros.promos_perks_api.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public UserServiceImpl(UserRepository userRepository) {
+
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
     public User createUser(User user) {
+
+        String hashedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
         return userRepository.save(user);
     }
 
