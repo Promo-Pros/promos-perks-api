1981adc4d09b59e0e0d0d811293daec90a92c088
Tiffany <139176913+talph101@users.noreply.github.com>
2025-01-21 10:26:08 -0600

    upated UserServiceImpl

--- src/main/java/com/Promo_pros/promos_perks_api/service/servicesImpl/UserServiceImpl.java
+++ src/main/java/com/Promo_pros/promos_perks_api/service/servicesImpl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.Promo_pros.promos_perks_api.entity.User;
 import com.Promo_pros.promos_perks_api.repository.UserRepository;
 import com.Promo_pros.promos_perks_api.service.UserService;
 import com.Promo_pros.promos_perks_api.util.BCryptUtil;
+import com.Promo_pros.promos_perks_api.util.JwtTokenUtil;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -11,10 +12,14 @@ import java.util.Set;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final JwtTokenUtil jwtTokenUtil;
     private UserRepository userRepository;
+    private BCryptUtil bCryptUtil;
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.bCryptUtil = bCryptUtil;
     }
 
     @Override
@@ -23,9 +28,17 @@ public class UserServiceImpl implements UserService {
             user.setRoles(Set.of("ROLE_USER")); //set to a default role
         }
         //Hash the password
-//        String hashedPassword = BCryptUtil.generatedSecurePassword(user.getPassword());
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+        String hashedPassword = BCryptUtil.generatedSecurePassword(user.getPassword());
+        user.setPassword(hashedPassword);
+
+
+      User savedUser = userRepository.save(user);
+
+      String token = jwtTokenUtil.generateAccessToken(savedUser);
+      // This will be used if we want new users to be automatically logged in
+//      savedUser.setToken(token);
+
+      return savedUser;
     }
 
     @Override
@@ -42,7 +55,8 @@ public class UserServiceImpl implements UserService {
     public String loginUser(User user) {
         User existingUser =  userRepository.findByEmail(user.getEmail()).orElse(null);
         if (existingUser != null && BCryptUtil.matchPasswords(user.getPassword(), existingUser.getPassword())) {
-            return "You have logged in";
+            String token = jwtTokenUtil.generateAccessToken(existingUser);
+            return token;
         }
         return "Invalid Login";
 
