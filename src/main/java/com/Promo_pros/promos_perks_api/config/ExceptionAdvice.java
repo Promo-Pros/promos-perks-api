0c0383ca71f9d18542324f6e5c7b4de9d1592af9
Tiffany <139176913+talph101@users.noreply.github.com>
2025-01-26 19:42:45 -0600

    created and refactored packages for new jwt implementation. Added code for Constants class.

---
+++ src/main/java/com/Promo_pros/promos_perks_api/config/ExceptionAdvice.java
@@ -0,0 +1,4 @@
+package com.Promo_pros.promos_perks_api.config;
+
+public class ExceptionAdvice {
+}
---
+++ src/main/java/com/Promo_pros/promos_perks_api/controller/AuthenticationContoller.java
@@ -0,0 +1,4 @@
+package com.Promo_pros.promos_perks_api.controller;
+
+public class AuthenticationContoller {
+}
